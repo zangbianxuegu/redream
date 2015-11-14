d29c9b2ae61d9b7dd2a4233e7bf96a9851bc9a73
chenzhao <chenzhao@jiandanxinli.com>
2015-11-14 19:36:40 +0800

    Test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
-    assert_response :success  
+    assert_response :success
     assert_select "title", "Redream your dream"
   end
 
